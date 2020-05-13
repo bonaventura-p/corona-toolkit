43a9ad77c15a03c0efc1bdbbb064343847f590f2
bonaventura-p <bonaventurapacileo@gmail.com>
2020-05-13 21:30:41 +0000

    new data structure, logo

--- helpers/.ipynb_checkpoints/aggregators-checkpoint.py
+++ helpers/.ipynb_checkpoints/aggregators-checkpoint.py
@@ -1,7 +1,6 @@
 
 
 #missing features:
-# new data structure from Niek
 # headset id and first session both oin the pdf and as filename
 # add chart to show activity over time
 # add logo?
@@ -17,12 +16,12 @@ from helpers.apicallers import getJsonData, MailgunClient
 def daily_overview ():
     '''add descr'''
 
-    data = getJsonData(days_ago=7) #all patients
+    data = getJsonData(days_ago=7, env='PROD') #all patients
 
     path = "/tmp/"
 
     for patient in data['data']:
-        html_out, outfile = __corona_report__(data=patient, keys=coronaDict)
+        html_out, outfile = __corona_report__(data=patient, keys=coronaDict, html_email_template = html_email_template)
         convert_to_pdf(html_out, path + outfile) #add some return bool if success
 
         if MailgunClient().send_mail(
@@ -32,43 +31,65 @@ def daily_overview ():
             print("Sending daily report failed!")
 
 
-def __corona_report__(data: Dict[str, Any], keys: Dict[Any, Any], appname: str = 'tech.syncvr.fysio', lang: str = 'dutch') -> str:
+def __corona_report__(data: Dict[str, Any], keys: Dict[Any, Any], html_email_template: str, appname: str = 'tech.syncvr.fysio', lang: str = 'dutch') -> str:
     '''add description'''
 
     patient_html: str = ""
+    
+    headset_html: str = ""
+    
+    #print(data['patientId'])
 
-    patient_html: str = __title_row__([keys['appname'][appname][lang]])
+    patient_html += __title_row__([keys['appname'][appname][lang]])
 
-    for session in data['results'][appname]:
+    patient_html += __summary_row__([keys['date'][lang]+ ": " +dateTransformer(data['results'][appname]['firstResultStartTime'])],header='h2')
 
-        date = time.ctime(session['start_time'] / 1000).split()
 
-        date.pop(3)  # remove time
-
-        date = str(date[0] + " " + date[1] + " " + date[2] + " " + date[3])
-
-        patient_html += __header_row__([date, "", ""])
-
-        patient_html += __subheader_row__([keys['exercise_code'][session['exercise_code']]['name'][lang]])
+    for session in data['results'][appname]['data']: 
+        #print(session)
+        
+        patient_html += __header_row__([dateTransformer(session['start_time']),keys['exercise_code'][session['exercise_code']]['name'][lang]])
 
         patient_html += __data_row__([keys['difficulty_level']['name'][lang],
-                                      keys['difficulty_level']['levels'][lang][session['difficulty_level']], ""])
+                                      keys['difficulty_level']['levels'][lang][session['difficulty_level']]])
 
-        patient_html += __data_row__([keys['duration']['name'][lang], str(int(session['duration'] / 60)), #CONVERT DURATION AS WEEKLY UPDATE IN 00:00:00
-                                      keys['duration']['unit'][lang]])
+        patient_html += __data_row__([keys['duration']['name'][lang], str(datetime.timedelta(seconds=session['duration']))])
+                                      #,keys['duration']['unit'][lang]])
 
         for action in session['score']:
             patient_html += __data_row__([keys['exercise_code'][session['exercise_code']]['scores'][lang][action],
-                                          str(session['score'][action]), ""])
-
-    html_out = html_email_template.format(patient_html)
-    outfile = data['patientId'] + "_" + str(datetime.datetime.now().date()) + '.pdf'
+                                          str(session['score'][action])])
+    
+    for session in data['results'][appname]['data']: 
+        headset_html += __summary_row__([dateTransformer(session['start_time']) + " Headset: {}".format(session['headsetHumanReadableName']) + " (ID: {})".format(session['headsetId'])], header='p')
+    
+    html_out = html_email_template.format(patient_html, headset_html)
+    outfile = data['patientId'] + "_" + str(datetime.datetime.now().date()) + '.pdf' #headset not unique to patient. start time not unique as filename. patient+date unique combination
 
     return html_out, outfile
 
 
+def dateTransformer(timestamp: int, ms: bool = True) -> str:
+    'input is timestamp, ms if milliseconds,  output is date'
+    if ms:
+        timestamp /= 1000
+    else:
+        print('{} is not treated as milliseconds'.format(timestamp))
+    
+    date = time.ctime(timestamp).split()
+
+    date.pop(3)  # remove time
+    
+    date = str(date[0] + " " + date[1] + " " + date[2] + " " + date[3]) #could remove weekday
+
+    return date 
+
+
 def convert_to_pdf(body_html: str, outfile: str):
-    return HTML(string=body_html).write_pdf(outfile, stylesheets=["helpers/style.css"])
+    #make it return a bool if it went smooth
+    
+    
+    return HTML(string=body_html, base_url="./images/").write_pdf(outfile, stylesheets=["helpers/style.css"])
 
 
 def __title_row__(data: List[str]) -> str:
@@ -106,6 +127,23 @@ def __subheader_row__(data: List[str]) -> str:
     html += "</tr>"
     return html
 
+def __summary_row__(data: List[str], header: str) -> str:
+    
+    if header == "p":
+        html: str = "<tr class='border' >"
+    else:
+        html: str = ""
+    
+    header = "<"+header+">"
+    
+    for s in data:
+        html += header + s + header
+    
+    if header == "p":
+        html += "</tr>"
+    
+    return html
+
 
 coronaDict= {'exercise_code':{
     'sorting':{'name':{'english':'Sorting','dutch':'Sorteren'},'scores':{'dutch':{'sorting_objects_total':'Objecten totaal','sorting_objects_missed':'Objecten gemist',
@@ -130,7 +168,7 @@ coronaDict= {'exercise_code':{
 'bananas_collected':'Bananas collected',
 'strawberries_collected':'Strawberries collected'}}}}, 'difficulty_level':{'name':{'english':'Difficulty level', 'dutch':'Moeilijkheidsgraad'},'levels':{'dutch':{1:'Heel Makkelijk', 2: 'Makkelijk',3: 'Gemiddeld', 4: 'Moeilijk',5:'Heel Moeilijk'},
     'english':{1:'Very easy',2: 'Easy', 3: 'Average',4: 'Difficult', 5:'Very difficult'}}},
-            'duration':{'name':{'english':'Duration','dutch':'Duur'},'unit':{'dutch':'minuten','english':'minutes'}},'appname':{'tech.syncvr.fysio':{'dutch':'Fitter Vandaag', 'english':'Fitter Today'}}}
+            'duration':{'name':{'english':'Duration','dutch':'Duur'},'unit':{'dutch':'uren:minuten:seconden','english':'hours:minutes:seconds'}},'appname':{'tech.syncvr.fysio':{'dutch':'Fitter Vandaag', 'english':'Fitter Today'}},'date':{'english':'First session on','dutch':'Eerste sessie op'}}
 
 
 html_email_template: str = """
@@ -153,6 +191,7 @@ html_email_template: str = """
 		</style>	
 	</head>
 	<body>
+    <img src="FitterVandaagLogo.png" style="float: right; width: 15%">
 	    <table>
             {}
         </table>
